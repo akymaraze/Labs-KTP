6752ecd75a09b43714055548a9ccad211fff3d54
akymaraze <akymaraze@gmail.com>
2026-06-04 16:05:59 +0300

    Delete лабы джава directory

--- лабы джава/lab4/CustomEmailFormatException.java
+++
@@ -1,7 +0,0 @@
-package lab4;
-//CustomEmailFormatException.java
-public class CustomEmailFormatException extends Exception {
- public CustomEmailFormatException(String message) {
-     super(message);
- }
-}
